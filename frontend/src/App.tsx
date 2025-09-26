a820d404206113e064dc83914a23fa9a877e54b8
GMonchain <gmonchain@gmail.com>
2025-09-26 11:07:23 +0700

    Commit 9: Home Page Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,12 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home />
     </div>
   );
 }
