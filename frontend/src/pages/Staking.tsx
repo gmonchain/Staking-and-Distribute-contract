82dd11dbee95102ab887db76f4e143a7e9e4b3f8
GMonchain <gmonchain@gmail.com>
2025-09-26 11:07:30 +0700

    Commit 10: Staking Page (Placeholder)

---
+++ frontend/src/pages/Staking.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Staking: React.FC = () => {
+  return (
+    <div style={{
+      padding: '2rem',
+      textAlign: 'center'
+    }}>
+      <h2>Staking Page</h2>
+      <p>This is where staking functionalities will be implemented.</p>
+    </div>
+  );
+};
+
+export default Staking;
