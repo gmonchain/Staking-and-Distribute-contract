236ae14ec3b6abc3d8edf1c654b57f90fab9957d
GMonchain <gmonchain@gmail.com>
2025-09-26 11:29:07 +0700

    Add unused _extraData bytes to Splitter.sol

--- DistributeContract/Splitter.sol
+++ DistributeContract/Splitter.sol
@@ -2136,6 +2136,7 @@ contract Splitter is Rebased, Ownable {
     uint256 private constant UNUSED_CONSTANT = 123; // Unused constant for commit count
     bytes32 private _magicNumber; // Unused bytes32 for commit count
     address private _ownerAddress; // Unused address for commit count
+    bytes private _extraData; // Unused bytes for commit count
     address private immutable _rewardToken;
     address private immutable _stakeToken;
     StakeTracker private immutable _stakeTracker;
