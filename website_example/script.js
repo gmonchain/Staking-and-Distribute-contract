60ff6bc79cf06689f4dfd086975816449dc94250
GMonchain <gmonchain@gmail.com>
2025-09-26 10:52:03 +0700

    feat(website): Add web3 and connectWallet function to script.js

--- website_example/script.js
+++ website_example/script.js
@@ -4,3 +4,19 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log('Website loaded');
     // Further JavaScript logic will go here
 });
+
+let web3;
+
+async function connectWallet() {
+    if (window.ethereum) {
+        try {
+            await window.ethereum.request({ method: 'eth_requestAccounts' });
+            web3 = new Web3(window.ethereum);
+            console.log('MetaMask connected:', web3);
+        } catch (error) {
+            console.error('User denied account access or other error:', error);
+        }
+    } else {
+        console.error('MetaMask is not installed. Please consider installing it!');
+    }
+}
