0c8090cef9daa8d5cdd69132b6ef4a05d049b1ea
GMonchain <gmonchain@gmail.com>
2025-09-26 11:06:02 +0700

    Commit 4: Connect Wallet Button (UI)

---
+++ frontend/src/components/ConnectWalletButton.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ConnectWalletButton: React.FC = () => {
+  return (
+    <button>
+      Connect Wallet
+    </button>
+  );
+};
+
+export default ConnectWalletButton;
