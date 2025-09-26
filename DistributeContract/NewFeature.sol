cc8d55d23ccb3f753c5099edadb5e2a21cb19af7
GMonchain <gmonchain@gmail.com>
2025-09-26 11:13:30 +0700

    feat: Apply onlyOwner modifier to setOwner function

--- DistributeContract/NewFeature.sol
+++ DistributeContract/NewFeature.sol
@@ -27,7 +27,7 @@ contract NewFeature {
 
     event NumberUpdated(uint oldNumber, uint newNumber);
 
-    function setOwner(address _newOwner) public {
+    function setOwner(address _newOwner) public onlyOwner {
         require(_newOwner != address(0), "New owner cannot be the zero address");
         owner = _newOwner;
     }
