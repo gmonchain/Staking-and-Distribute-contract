dbb15cde1b308494f034431c502a4531189cb910
GMonchain <gmonchain@gmail.com>
2025-09-26 11:12:24 +0700

    feat: Add setMyNumber function to NewFeature contract

--- DistributeContract/NewFeature.sol
+++ DistributeContract/NewFeature.sol
@@ -3,4 +3,8 @@ pragma solidity ^0.8.20;
 contract NewFeature {
     uint public myNumber;
     // Initial content for the new feature contract
+
+    function setMyNumber(uint _newNumber) public {
+        myNumber = _newNumber;
+    }
 }
