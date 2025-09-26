33c2ca9c8ab99ae27d66f2b895f8d9355045a2d2
GMonchain <gmonchain@gmail.com>
2025-09-26 11:45:33 +0700

    DOC: Add NatSpec comments to onlyRebase and onlyDistributor modifiers

--- DistributeContract/Splitter.sol
+++ DistributeContract/Splitter.sol
@@ -2160,11 +2160,17 @@ contract Splitter is Rebased, Ownable {
      */
     event Unpaused(address account);
 
+    /**
+     * @dev Throws if called by any account other than the Rebase contract.
+     */
     modifier onlyRebase {
         require(msg.sender == _rebase, "Only Rebase");
         _;
     }
 
+    /**
+     * @dev Throws if called by any account other than an authorized distributor.
+     */
     modifier onlyDistributor {
         require(_distributors.contains(msg.sender), "Only Distributor");
         _;
