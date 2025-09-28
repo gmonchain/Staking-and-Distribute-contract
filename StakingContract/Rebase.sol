18dda8d43b33fa2168e84ad606532b870824228e
GMonchain <gmonchain@gmail.com>
2025-09-28 16:37:24 +0700

    Add comment to _stake internal function

--- StakingContract/Rebase.sol
+++ StakingContract/Rebase.sol
@@ -2017,6 +2017,7 @@ contract Rebase is ReentrancyGuard {
     }
 
     function _stake(address app, address token, uint quantity) internal {
+        // Internal function to handle staking logic.
         User storage user = _users[msg.sender];
         (,uint userStake) = user.appTokenStakes[app].tryGet(token);
         (,uint appStake) = _appTokenStakes[app].tryGet(token);
