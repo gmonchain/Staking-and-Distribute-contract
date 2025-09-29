24fb655a809d0fccaa9b46efb1d03df754d675f8
GMonchain <gmonchain@gmail.com>
2025-09-29 10:31:24 +0700

    Change UNRESTAKE_GAS_LIMIT to private

--- StakingContract/Rebase.sol
+++ StakingContract/Rebase.sol
@@ -1996,7 +1996,7 @@ contract Rebase is ReentrancyGuard {
     address public constant _WETH = 0x4200000000000000000000000000000000000006; // WETH address on Optimism
     address public immutable _clonableToken; // Address of the clonable ReToken contract
 
-    uint public constant UNRESTAKE_GAS_LIMIT = 1000000;
+    uint private constant UNRESTAKE_GAS_LIMIT = 1000000;
 
     event Stake (
         address indexed user,
